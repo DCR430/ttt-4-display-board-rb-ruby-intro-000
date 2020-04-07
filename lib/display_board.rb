5c8ae46c0a4efc42d653398c501e43fce2132016
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-07 22:23:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,24 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
+def display_board(board)
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
-end
-
-def display_board
  puts "   |   |   "
  puts "-----------"
  puts "   | X |   "
  puts "-----------"
  puts "   |   |   "
-end
+ puts " O |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts " O  |   |  "
+ puts "-----------"
+ puts "   | X  |  "
+ puts "-----------"
+ puts "   |   |   "
+ end
