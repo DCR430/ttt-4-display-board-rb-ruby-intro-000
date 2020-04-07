1d20ed0730d5c1bec76a8974a03999b6ec866640
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-07 22:25:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,16 +6,19 @@ def display_board(board)
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
+
  puts "   |   |   "
  puts "-----------"
  puts "   | X |   "
  puts "-----------"
  puts "   |   |   "
+
  puts " O |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
+
  puts " O  |   |  "
  puts "-----------"
  puts "   | X  |  "
